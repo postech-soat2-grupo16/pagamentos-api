c3e9a9e24c37115bee5af0993521f29c07d7eccb
Pedro Haramoto <pedroharamoto@gmail.com>
2024-01-21 17:34:44 -0300

    fix health check

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "tg_pagamentos_api" {
     enabled             = true
     interval            = 30
     matcher             = "200-299"
-    path                = "/pagamentos/healthcheck"
+    path                = "/pagamentos/health"
     port                = "traffic-port"
     protocol            = "HTTP"
     timeout             = 5
